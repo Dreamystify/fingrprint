5cebaea54b56492a759ed962380d92a5424a37bf
Dreamystify <support@dreamystify.com>
2022-06-16 13:00:01 +1000

    Add integration tests

---
+++ test/index.spec.ts
@@ -0,0 +1,39 @@
+import Fingrprint from '../src/index';
+import chai, { assert, expect } from 'chai';
+import 'mocha';
+
+chai.use(require('chai-as-promised'));
+
+describe('Fingrprint getIds function', () => {
+
+    it('should return an array of bigint', async () => {
+        const fingrprint: any = new Fingrprint(`localhost`, 6389, `default`, `passwd`);
+        const ids = await fingrprint.getIds(1);
+        expect(ids).to.have.lengthOf(1);
+        assert.typeOf(ids[0], 'BigInt');
+        await fingrprint.close();
+    });
+
+    it('should fail authentication to redis server', async () => {
+        try {
+            const fingrprint: any = new Fingrprint(`localhost`, 6389, `default`, `passwd`);
+            const ids = await fingrprint.getIds(1);
+            await fingrprint.close();
+        } catch(e) {
+            if (e instanceof Error) {
+                expect(e.message).to.equal('WRONGPASS invalid username-password pair');
+            }
+        }
+    });
+
+    it('should fail to connect to redis server', async () => {
+        try {
+            const fingrprint: any = new Fingrprint(`local`, 8888, `default`, `passwd`);
+            const ids = await fingrprint.getIds(1);
+        } catch(e) {
+            if (e instanceof Error) {
+                expect(e.message).to.equal('getaddrinfo ENOTFOUND local');
+            }
+        }
+    });
+});
