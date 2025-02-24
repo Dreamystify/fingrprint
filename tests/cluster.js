e2bda47c93a6e133d23705ba5737822600cdc1cb
Corey Lylyk <corey.lylyk@inquisitive.com>
2025-02-25 07:00:53 +1100

    Add sentinel and cluster tests

---
+++ tests/cluster.js
@@ -0,0 +1,32 @@
+const { Fingrprint } = require('../lib/index');
+
+const config = {
+  clusterNodes: [
+    { host: 'redis-cluster-node1', port: 6379 },
+    { host: 'redis-cluster-node2', port: 6379 },
+    { host: 'redis-cluster-node3', port: 6379 },
+  ],
+  username: 'default',
+  password: 'fingrprint',
+  database: 0,
+};
+
+setTimeout(async () => {
+  console.log('Starting Cluster test 10000ms...');
+  console.log('Configuration:', config);
+
+  try {
+    const fingrprint = await Fingrprint.initialize(config);
+
+    // Generate a batch of 3 unique IDs.
+    const ids = await fingrprint.getIds(3);
+    console.log('Generated IDs:', ids);
+    console.log('Test Success');
+
+    process.exit(0);
+  } catch (error) {
+    console.error('Error connecting to Cluster:', error);
+    process.exit(1);
+  }
+}, 20000); // wait 20 seconds
+
---
+++ tests/sentinel.js
@@ -0,0 +1,33 @@
+const { Fingrprint } = require('../lib/index');
+
+const config = {
+  sentinels: [
+    { host: 'redis-sentinel', port: 26379 },
+  ],
+  name: 'mymaster',
+  username: 'default',
+  password: 'fingrprint',
+  sentinelUsername: 'default',
+  sentinelPassword: 'fingrprint',
+  connectTimeout: 10000,
+};
+
+setTimeout(async () => {
+  console.log('Starting Sentinel test 2000ms...');
+  console.log('Configuration:', config);
+
+  try {
+    const fingrprint = await Fingrprint.initialize(config);
+
+    // Generate a batch of 3 unique IDs.
+    const ids = await fingrprint.getIds(3);
+    console.log('Generated IDs:', ids);
+    console.log('Test Success');
+
+    process.exit(0);
+  } catch (error) {
+    console.error('Error connecting to Sentinel:', error);
+    process.exit(1);
+  }
+}, 2000); // wait 2 seconds
+
